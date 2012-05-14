3450e3332f2659168212890c979dc95bf018603f
leesbian <lee@leesbian.net>
2012-05-14 15:32:17 +0100

    bumped to RC-1

--- app/Mage.php
+++ app/Mage.php
@@ -172,7 +172,7 @@ final class Mage
             'minor'     => '0',
             'revision'  => '0',
             'patch'     => '0',
-            'stability' => 'BETA',
+            'stability' => 'RC-1',
             'number'    => '',
         );
     }

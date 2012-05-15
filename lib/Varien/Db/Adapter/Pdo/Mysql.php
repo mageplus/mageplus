39e49b5679ff5d0523a306ac48f20a8970907970
Daniel Fahlke <daniel@digitalmanufaktur.com>
2012-05-15 10:23:49 +0200

    fix some zend coding style errors

--- lib/Varien/Db/Adapter/Pdo/Mysql.php
+++ lib/Varien/Db/Adapter/Pdo/Mysql.php
@@ -2492,9 +2492,8 @@ class Varien_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements V
             $ddl = array();
 
             $sql = 'SHOW TRIGGERS WHERE `table` = :table';
-            $bind =  array( 'table'=> $this->_getTableName($tableName) );
-            foreach ($this->fetchAll($sql, $bind ) as $row)
-            {
+            $bind =  array( 'table' => $this->_getTableName($tableName) );
+            foreach ($this->fetchAll($sql, $bind) as $row) {
                 $triggerName      = 'Trigger';
                 $triggerEvent     = 'Event';
                 $triggerTable     = 'Table';
@@ -2502,8 +2501,7 @@ class Varien_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements V
                 $triggerTiming    = 'Timing';
 
                 $upperKeyName = strtoupper($row[$triggerName]);
-                if (!isset($ddl[$upperKeyName]))
-                {
+                if (!isset($ddl[$upperKeyName])) {
                     $ddl[$upperKeyName] = array(
                         'SCHEMA_NAME'        => $schemaName,
                         'TABLE_NAME'         => $tableName,

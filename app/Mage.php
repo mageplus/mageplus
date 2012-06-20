c8f334897df962889af5e9f29fcca43372d33929
Lee Bolding <lee@leesbian.net>
2012-06-20 15:05:10 +0100

    updated release notes and bumped version

--- app/Mage.php
+++ app/Mage.php
@@ -169,9 +169,9 @@ final class Mage
     {
         return array(
             'major'     => '1',
-            'minor'     => '0',
+            'minor'     => '1',
             'revision'  => '0',
-            'patch'     => '1',
+            'patch'     => '0',
             'stability' => '',
             'number'    => '',
         );
